7f427d4a9c5de2cb566f31bf854b15dec15b8f04
David Orbelian <david.orbelian@gmail.com>
2020-06-02 20:42:59 +0400

    Add Runtime View Primitives.

---
+++ Runtime/Views/Primitives/TextView.cs
@@ -0,0 +1,23 @@
+﻿using Gamgaroo.MVVM.Runtime.Abstractions;
+using UnityEngine.UI;
+
+namespace Gamgaroo.MVVM.Runtime.Views.Primitives
+{
+    public sealed class TextView : Text, IView<string>, IView<float>, IView<int>
+    {
+        public void Set(string viewModel)
+        {
+            text = viewModel;
+        }
+
+        public void Set(float viewModel)
+        {
+            text = viewModel.ToString();
+        }
+
+        public void Set(int viewModel)
+        {
+            text = viewModel.ToString();
+        }
+    }
+}
