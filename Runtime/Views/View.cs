da43fe3504f88c294fca7b76dfb3670c72af0933
David Orbelian <david.orbelian@gmail.com>
2020-06-08 16:53:04 +0400

    Add View base class (fixes #4).

---
+++ Runtime/Views/View.cs
@@ -0,0 +1,23 @@
+﻿using Gamgaroo.View.Runtime.Abstractions;
+using UnityEngine;
+
+namespace Gamgaroo.View.Runtime.Views
+{
+    public abstract class View<TViewModel> : MonoBehaviour, IView<TViewModel>
+    {
+        private TViewModel _current;
+
+        public void Set(TViewModel viewModel)
+        {
+            if (_current != null)
+                Unbind(_current);
+
+            _current = viewModel;
+
+            Bind(_current);
+        }
+
+        protected abstract void Bind(TViewModel viewModel);
+        protected abstract void Unbind(TViewModel viewModel);
+    }
+}
